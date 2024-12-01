21e5f9865911a6bf7c10fadc2eb10045044a8f9b
Ben Gican <b3nhur77@gmail.com>
2024-12-01 16:46:41 +0800

    update

--- src/services/img-scraper.ts
+++ src/services/img-scraper.ts
@@ -1,7 +1,20 @@
 'use server';
 import { JSDOM } from 'jsdom';
-import puppeteer from 'puppeteer-extra';
+import puppeteer from 'puppeteer-core';
 import probe from 'probe-image-size';
+import chrome from 'chrome-aws-lambda';
+
+// let chrome: any = {};
+// let puppeteer: any;
+
+// if (process.env.ENV === 'prod') {
+//   chrome = require('chrome-aws-lambda');
+//   puppeteer = require('puppeteer-core');
+// } else {
+//   puppeteer = require('puppeteer');
+// }
+
+let options = {};
 
 const ELEMENTS_TO_REMOVE = ['script', 'noscript', 'style', 'header', 'footer', 'nav', 'aside', 'label', 'input', 'select', 'option', 'link', 'iframe', 'meta'];
 
@@ -46,8 +59,16 @@ async function getImageDimensions(url: string): Promise<{ url: string; width: nu
 }
 
 async function scrapeWithPuppeteer(url: string): Promise<string> {
+  options = {
+    args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+    defaultViewport: chrome.defaultViewport,
+    executablePath: await chrome.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+  };
+
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
 
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36');
